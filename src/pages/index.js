434d7e946e1fb48ed0227499ee288ee281618a74
root <root@MR-ISRAEL.localdomain>
2021-04-14 15:38:05 -0300

    configuração material-ui

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Button } from '@material-ui/core';
 
 export default function Home() {
   return (
     <div>
       Tubers, aprendendo next.js + material-ui
+       <Button color="primary">Hello World</Button>
     </div>
   );
 }
